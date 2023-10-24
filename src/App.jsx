1521a68edf3fcd923fe2f17a65f6fc6319c1cd8e
cliff <cliffmudigo5@gmail.com>
2023-10-24 19:45:48 +0300

    :sparkles: feat: added neccessary files

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from './components/Layout/Navbar'
+import About from './components/Pages/About'
+import Home from './components/Pages/Home'
+import Landingpage from './components/Pages/Landingpage'
+import Cart from './components/Pages/Cart'
+import Onsale from './components/Pages/Onsale'
+import Contact from './components/Pages/Contact'
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <div>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/onsale" element={<Onsale />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/contact" element={<Landingpage />} />
+          </Routes>
+        </div>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
